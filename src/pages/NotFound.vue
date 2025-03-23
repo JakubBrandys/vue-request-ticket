5079e6067eea7056107c017bbac8f3a657faee6d
Jakub Brandys <kuba.brandys99@gmail.com>
2025-03-23 18:21:54 +0100

    feat(VRT-01): create NotFound page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>404 - Page Not Found</h1>
+    <p>The page you're looking for doesn't exist.</p>
+    <router-link to="/">Go to Home</router-link>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+div {
+  text-align: center;
+  padding: 4rem 2rem 2rem;
+}
+
+a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
